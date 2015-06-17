6b9e740fb6c69ce4bd430c0e5ac27ce2d1f014c8
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-06-17 16:07:16 -0300

    Botão salvar implementado

--- CollegeManager/CollegeManager/CadastroViewController.swift
+++ CollegeManager/CollegeManager/CadastroViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 
 class CadastroViewController: UINavigationController{
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.whiteColor()
@@ -19,7 +23,28 @@ class CadastroViewController: UINavigationController{
     @IBAction func didPressSaveButton(sender: UIBarButtonItem) {
         //implement save methods
         
+        let disciplinaManager = DisciplinaManager.sharedInstance
+        let avaliacaoManager = AvaliacaoManager.sharedInstance
+        
+        //Nova disciplina
+        var newDisciplina = disciplinaManager.novaDisciplina()
+        newDisciplina.nome = subjectLabel.text!
+        
+        var avaliacao = avaliacaoManager.novaAvaliacao()
+        avaliacao.nome = nameLabel.text!
+        avaliacao.nota = 6                                      // Falta adicionar
+        avaliacaoManager.salvar()
+        
+        newDisciplina.addAvaliacao(avaliacao)
+        
+        // Teste pra exibir --------
+        let ss = avaliacaoManager.buscarAvaliacoes()[0].nome
+        let rr = avaliacaoManager.buscarAvaliacoes()[0].nota
+        println("\(ss) - \(rr)")
+        // --------------
+        
         //Alert with some message
+        
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     @IBAction func didPressBackButton(sender: UIBarButtonItem) {
